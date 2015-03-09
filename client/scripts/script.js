fc4ec3c3df6398c72e1aac94ed67cbef10d710a9
sunyang730 <sunyang730@gmail.com>
2015-03-09 12:03:57 -0700

    Add: checkUser function to check if user is admin

---
+++ client/scripts/script.js
@@ -0,0 +1,7 @@
+var checkUser = function(){
+  var username = 'admin';
+  var password = 'password';
+  if(username === 'admin' && password === 'password'){
+    window.location = 'file:///Users/DennsYang/HackReactor/2015-02-solo/client/dashboard.html';
+  }
+};
