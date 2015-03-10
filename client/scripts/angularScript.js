7758473dea9d06cc715e12df20d4aa1f7cae480d
sunyang730 <sunyang730@gmail.com>
2015-03-09 17:10:54 -0700

    Ref: client with angular Add: sign up function for form

---
+++ client/scripts/angularScript.js
@@ -0,0 +1,23 @@
+var app = angular.module('ChatWithMe', ['firebase']);
+app.controller('signup', ['$scope', '$firebaseArray', function($scope, $firebaseArray){
+  var ref = new Firebase("https://chatwithmehr.firebaseio.com/");
+  $scope.user = {};
+  
+  $scope.signup = function(){
+    ref.createUser({
+      email: $scope.user.email,
+      password: $scope.user.password
+
+    }, function(error, userData){
+      if(error){
+        console.log("Error creating user", error.message);
+      } else {
+        console.log("Successfully created user account with uid: ", userData.uid);
+      }
+    });
+    
+  };
+
+  
+}]);
+
